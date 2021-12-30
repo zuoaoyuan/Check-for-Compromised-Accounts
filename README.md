8868f38034c9591815680af1b9756b68553e3487
Thomas Zuo <zuoaoyuan@live.com>
2021-12-30 02:08:08 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# Check-for-Compromised-Accounts
-Created with CodeSandbox
+Checking-for-Compromised-Accounts
+
+Overview:
+We want to inform our users if their account has been involved in a security breach on another site since their last login. Since people sometimes reuse passwords across multiple sites, we'll encourage them to update their information in case their password is no longer secure.
+
+Our goal:
+Hitting an external breach data API called Hack Check.
+Look for recent breaches that involve passwords. Limit the breaches in sampleResponse to breaches where:
+
+- IsSensitive is false
+- DataClasses array contains the value 'Passwords'
+- AddedDate is after the user's lastLogin
+
+Suggest password change:
+Now that we know the recent breaches the user has been involved in, we want to show a warning on their Dashboard if they have been involved in any breaches.
+When the user logs in, if they have any breaches, show an Alert card with listed breaches information fetched from the API response.
+
+Dismiss button on Alert Card:
+The button to dismiss in the Alert dialog should dismiss the alert for the rest of the user's session.
+
+No alerts:
+If no recent breaches being found or the alert has been dismissed, show "No alerts" inside the card body.
+
+Test users provided:
+You can use the following accounts to test, which are located in src/sample/users.js. The safe user is to return no breaches, and the unsafe user is to return breaches.
+
+User with no breaches ("Safe User")
+Email: safe@example.com
+Password: pw
+
+User with breaches ("Unsafe User")
+Email: test@example.com
+Password: pw
---
+++ src/components/App.jsx
@@ -0,0 +1,81 @@
+import React, { useState, useCallback } from "react";
+
+import LoginForm from "./LoginForm";
+import Dashboard from "./Dashboard";
+
+export default function App() {
+  const [auth, setAuth] = useState({
+    token: "",
+    user: {},
+    meta: {}
+  });
+
+  const onLoginSuccess = (auth) => {
+    setAuth(auth);
+  };
+
+  const onLogOut = (e) => {
+    setAuth({
+      token: "",
+      user: {},
+      meta: {}
+    });
+  };
+
+  const suggestPasswordChangeOff = useCallback(() => {
+    const newAuth = {
+      ...auth,
+      meta: {
+        suggestPasswordChange: false,
+        breachedAccounts: []
+      }
+    };
+
+    setAuth(newAuth);
+  }, [auth, setAuth]);
+
+  const currentPage = () => {
+    return auth.token ? (
+      <Dashboard
+        suggestPasswordChange={
+          auth.meta ? auth.meta.suggestPasswordChange : false
+        }
+        breaches={auth.meta ? auth.meta.breachedAccounts : null}
+        onDismissBreachAlert={suggestPasswordChangeOff}
+      />
+    ) : (
+      <LoginForm onLoginSuccess={onLoginSuccess} />
+    );
+  };
+
+  return (
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-info">
+        <a className="navbar-brand mr-auto" href="https://github.com/zuoaoyuan">
+          AppCo
+        </a>
+
+        {auth.token && (
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Tasks
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" onClick={onLogOut}>
+                Log out
+              </a>
+            </li>
+          </ul>
+        )}
+      </nav>
+      <div className="container">{currentPage()}</div>
+    </div>
+  );
+}
